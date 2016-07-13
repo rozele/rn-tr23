021df8c2454885382c76c9dd9897a8fc61133031
Eric Rozell <erozell@outlook.com>
2016-07-13 14:43:37 -0400

    Add ReactMapItemManager

---
+++ windows/ReactWindowsMap/Views/ReactMapItemManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ReactNative.UIManager;
+using Windows.UI.Xaml.Controls;
+using ReactNative.UIManager.Annotations;
+using Newtonsoft.Json.Linq;
+using Windows.Devices.Geolocation;
+using Windows.UI.Xaml.Controls.Maps;
+
+namespace ReactWindowsMap.Views
+{
+    class ReactMapItemManager : PanelViewParentManager<Canvas>
+    {
+        public override string Name
+        {
+            get
+            {
+                return "WindowsMapItem";
+            }
+        }
+
+        [ReactProp("location")]
+        public void SetLocation(Canvas view, JObject location)
+        {
+            var lat = location.Value<double>("lat");
+            var lon = location.Value<double>("lon");
+            var alt = location.Value<double>("alt");
+
+            var position = new BasicGeoposition
+            {
+                Latitude = lat,
+                Longitude = lon,
+                Altitude = alt,
+            };
+
+            MapControl.SetLocation(view, new Geopoint(position));
+        }
+
+        protected override Canvas CreateViewInstance(ThemedReactContext reactContext)
+        {
+            return new Canvas();
+        }
+    }
+}
