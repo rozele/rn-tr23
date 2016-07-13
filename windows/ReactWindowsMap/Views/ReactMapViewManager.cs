6ef7f28e5f467427e7c89efdc8eab789aff288ea
Eric Rozell <erozell@outlook.com>
2016-07-13 14:36:32 -0400

    Add ReactMapViewManager

---
+++ windows/ReactWindowsMap/Views/ReactMapViewManager.cs
@@ -0,0 +1,47 @@
+﻿using ReactNative.UIManager;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls.Maps;
+
+namespace ReactWindowsMap.Views
+{
+    class ReactMapViewManager : ViewParentManager<MapControl>
+    {
+        public override string Name
+        {
+            get
+            {
+                return "WindowsMapView";
+            }
+        }
+
+        public override void AddView(MapControl parent, DependencyObject child, int index)
+        {
+            parent.Children.Insert(index, child);
+        }
+
+        public override DependencyObject GetChildAt(MapControl parent, int index)
+        {
+            return parent.Children[index];
+        }
+
+        public override int GetChildCount(MapControl parent)
+        {
+            return parent.Children.Count;
+        }
+
+        public override void RemoveAllChildren(MapControl parent)
+        {
+            parent.Children.Clear();
+        }
+
+        public override void RemoveChildAt(MapControl parent, int index)
+        {
+            parent.Children.RemoveAt(index);
+        }
+
+        protected override MapControl CreateViewInstance(ThemedReactContext reactContext)
+        {
+            return new MapControl();
+        }
+    }
+}
