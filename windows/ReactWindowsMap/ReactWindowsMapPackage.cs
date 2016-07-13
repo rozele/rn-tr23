e7b2244ccd10d4ccc9f74adb34606a87a1870c70
Eric Rozell <erozell@outlook.com>
2016-07-13 14:45:49 -0400

    Adds ReactWindowsMapPackage

---
+++ windows/ReactWindowsMap/ReactWindowsMapPackage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using ReactNative.Bridge;
+using ReactNative.Modules.Core;
+using ReactNative.UIManager;
+using ReactWindowsMap.Views;
+
+namespace ReactWindowsMap
+{
+    public class ReactWindowsMapPackage : IReactPackage
+    {
+        public IReadOnlyList<Type> CreateJavaScriptModulesConfig()
+        {
+            return new List<Type>(0);
+        }
+
+        public IReadOnlyList<INativeModule> CreateNativeModules(ReactContext reactContext)
+        {
+            return new List<INativeModule>(0);
+        }
+
+        public IReadOnlyList<IViewManager> CreateViewManagers(ReactContext reactContext)
+        {
+            return new List<IViewManager>
+            {
+                new ReactMapViewManager(),
+                new ReactMapItemManager(),
+            };
+        }
+    }
+}
